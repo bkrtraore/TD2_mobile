9a82cee4cf38929228fcd0cea01618ca94add28a
bkrtraore <b.traore771@gmail.com>
2022-03-19 16:28:09 +0100

    Login OK / Register KO

--- app/src/main/java/com/example/td2/activityThree.java
+++ app/src/main/java/com/example/td2/activityThree.java
@@ -3,6 +3,7 @@ package com.example.td2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class activityThree extends AppCompatActivity {
 
--- app/src/main/java/com/example/td2/activityTwo.java
+++ app/src/main/java/com/example/td2/activityTwo.java
@@ -1,14 +1,60 @@
 package com.example.td2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class activityTwo extends AppCompatActivity {
+
+    private static final String CHANNEL_ID = "defaultChannel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        createNotificationChannel();
         setContentView(R.layout.activity_two);
     }
+
+    //Switch du layout 2 -> 3 en cliquant sur le bouton
+    public void TwoToThree(View view) {
+        setContentView(R.layout.activity_three);
+    }
+
+    //Switch du layout 3 -> 1 en cliquant sur le bouton
+    public void ThreeToOne(View view) {
+        setContentView(R.layout.activity_one);
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void Notify(View view) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                //.setSmallIcon(R.drawable.notification_icon)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("My notification")
+                .setContentText("Much longer text that cannot fit one line...")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+    }
+
 }
